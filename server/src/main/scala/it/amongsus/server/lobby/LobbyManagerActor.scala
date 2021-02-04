29ee65b913311d5310812a91d89c48a85c9df48b
Oleg Konchenkov <konchenkovoleg@gmail.com>
2021-02-04 10:30:09 +0100

    Update LobbyManagerActor.scala
    Implementer JoinPublicLobby logic in "Receive"

--- server/src/main/scala/it/amongsus/server/lobby/LobbyManagerActor.scala
+++ server/src/main/scala/it/amongsus/server/lobby/LobbyManagerActor.scala
@@ -26,5 +26,44 @@ class LobbyManagerActor extends Actor with IdGenerator with ActorLogging {
       context.watch(clientRef)
       clientRef ! Connected(clientId)
     }
+    case JoinPublicLobby(clientId, username, numberOfPlayers) => {
+      log.info(s"client $clientId wants to join a public lobby")
+      this.executeOnClientRefPresent(clientId) { ref =>
+        val lobbyType = PlayerNumberLobby(numberOfPlayers)
+        this.lobbyManger.addPlayer(GamePlayer(clientId, username, ref), lobbyType)
+        ref ! UserAddedToLobby()
+        this.checkAndCreateGame(lobbyType)
+      }
+    }
+
+  }
+  private def checkAndCreateGame(lobbyType: LobbyType): Unit = {
+    this.lobbyManger.attemptExtractPlayerForMatch(lobbyType) match {
+      case Some(players) => this.generateAndStartGameActor(lobbyType)(players)
+      case None =>
+    }
+  }
+
+  private def generateAndStartGameActor(lobbyType: LobbyType)(players: Seq[GamePlayer]): Unit = {
+    //game Actor creation
+    players.foreach(p => {
+      context.unwatch(p.actorRef)
+      // remove player form lobby
+      this.lobbyManger.removePlayer(p.id)
+      // remove player from connected players structure
+      this.connectedPlayers = this.connectedPlayers - p.id
+    })
+    //gameActor ! GamePlayers(players)
+  }
+
+  private def getClientRef(clientId: String): Option[ActorRef] = {
+    this.connectedPlayers.get(clientId)
+  }
+
+  private def executeOnClientRefPresent(clientId: String)(action: ActorRef => Unit): Unit = {
+    this.getClientRef(clientId) match {
+      case Some(ref) => action(ref)
+      case _ =>
+    }
   }
 }
