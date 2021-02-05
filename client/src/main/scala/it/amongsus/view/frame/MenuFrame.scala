2dafa037deb87efda05f7087a29a68a9a94933d5
Elia Pasqualini <elia.pasqualini@studio.unibo.it>
2021-02-05 22:28:06 +0100

    Update GUI in MenuFrame

--- client/src/main/scala/it/amongsus/view/frame/MenuFrame.scala
+++ client/src/main/scala/it/amongsus/view/frame/MenuFrame.scala
@@ -2,7 +2,7 @@ package it.amongsus.view.frame
 
 import akka.actor.ActorRef
 import cats.effect.IO
-import it.amongsus.view.swingio.{BorderFactoryIO, JFrameIO, JPanelIO}
+import it.amongsus.view.swingio.{BorderFactoryIO, JButtonIO, JFrameIO, JLabelIO, JPanelIO, JTextFieldIO}
 
 import java.awt.{BorderLayout, GridLayout}
 import javax.swing.JFrame
@@ -27,12 +27,36 @@ object MenuFrame {
 
     override def start(): IO[Unit] =
       for {
-        _ <- frame.setSize(WIDTH, HEIGHT)
-        _ <- frame.setVisible(true)
         menuPanel <- JPanelIO()
         menuBorder <- BorderFactoryIO.emptyBorderCreated(10, 10, 10, 10)
         _ <- menuPanel.setBorder(menuBorder)
         _ <- menuPanel.setLayout(new BorderLayout())
+        titlePanel <- JPanelIO()
+        titleBorder <- BorderFactoryIO.emptyBorderCreated(25, 200, 10, 200)
+        _ <- titlePanel.setBorder(titleBorder)
+        _ <- titlePanel.setLayout(new BorderLayout())
+        title <- JLabelIO()
+        _ <- title.setText("AMONG SUS")
+        _ <- titlePanel.add(title,BorderLayout.CENTER)
+        _ <- menuPanel.add(titlePanel,BorderLayout.NORTH)
+        inputPanel <- JPanelIO()
+        _ <- inputPanel.setLayout(new GridLayout(5,1))
+        nameField <- JTextFieldIO()
+        _ <- inputPanel.add(nameField)
+        joinPublic <- JButtonIO("Partecipa ad una partita pubblica")
+        _ <- inputPanel.add(joinPublic)
+        startPrivate <- JButtonIO("Crea una partita privata")
+        _ <- inputPanel.add(startPrivate)
+        codeField <- JTextFieldIO()
+        _ <- inputPanel.add(codeField)
+        joinPrivate <- JButtonIO("Partecipa ad una partita privata")
+        _ <- inputPanel.add(joinPrivate)
+        _ <- menuPanel.add(inputPanel,BorderLayout.SOUTH)
+        cp <- frame.contentPane()
+        _ <- cp.add(menuPanel)
+        _ <- frame.setResizable(false)
+        _ <- frame.setSize(WIDTH, HEIGHT)
+        _ <- frame.setVisible(true)
       } yield ()
   }
 
