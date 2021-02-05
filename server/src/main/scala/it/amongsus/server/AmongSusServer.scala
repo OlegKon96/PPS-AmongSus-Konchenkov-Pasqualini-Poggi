0773eee862c8a0e4bb6661c2b86ab39330668566
Oleg Konchenkov <konchenkovoleg@gmail.com>
2021-02-05 11:18:15 +0100

    Create Costants.scala
    It's a class containing constants used to connect to the server

--- server/src/main/scala/it/amongsus/server/AmongSusServer.scala
+++ server/src/main/scala/it/amongsus/server/AmongSusServer.scala
@@ -2,17 +2,18 @@ package it.amongsus.server
 
 import akka.actor.ActorSystem
 import com.typesafe.config.ConfigFactory
+import it.amongsus.Constants
 import it.amongsus.server.lobby.LobbyManagerActor
 
 object AmongSusServer extends App {
   val config = ConfigFactory.parseString(
     s"""
-       |akka.remote.netty.tcp.hostname = localhost
-       |akka.remote.netty.tcp.port = 5150
+       |akka.remote.netty.tcp.hostname = ${Constants.Remote.SERVER_ADDRESS}
+       |akka.remote.netty.tcp.port = ${Constants.Remote.SERVER_PORT}
        |""".stripMargin).withFallback(ConfigFactory.load())
 
-  val system = ActorSystem("AmongSusServer", config)
-  system.actorOf(LobbyManagerActor.props(), "lobby")
+  val system = ActorSystem(Constants.Remote.SERVER_ACTOR_SYSTEM_NAME, config)
+  system.actorOf(LobbyManagerActor.props(), Constants.Remote.SERVER_LOBBY_ACTOR_NAME)
 }
 
 
