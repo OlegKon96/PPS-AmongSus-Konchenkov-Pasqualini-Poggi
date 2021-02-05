4f80e7eff0df797a5401a1cfb71c62c1f35e68e1
Elia Pasqualini <elia.pasqualini@studio.unibo.it>
2021-02-05 12:51:50 +0100

    Update MainControllerImpl

--- client/src/main/scala/it/amongsus/controller/MainControllerImpl.scala
+++ client/src/main/scala/it/amongsus/controller/MainControllerImpl.scala
@@ -4,16 +4,17 @@ import it.amongsus.{ActorSystemManager, Constants}
 import it.amongsus.client.model.lobby.LobbyActor
 import it.amongsus.messages.LobbyMessagesClient.{ConnectClient, JoinPublicLobbyClient}
 import it.amongsus.model.LobbyActorInfo
+import it.amongsus.view.frame.MenuFrame
 
 class MainControllerImpl() extends MainController {
 
   private lazy val lobbyActorRef =
     ActorSystemManager.actorSystem.actorOf(LobbyActor.props(LobbyActorInfo()), "client-lobby")
-  //val view: ViewImpl = new ViewImpl(Option(lobbyActorRef))
+  val menuView: MenuFrame  =  MenuFrame(Option(lobbyActorRef))
 
   override def start(): Unit = {
     connect()
-    //view.test() unsafeRunSync
+    menuView.start() unsafeRunSync()
   }
 
   private def connect(): Unit = {
