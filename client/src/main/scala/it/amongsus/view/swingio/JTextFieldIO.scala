09cd78b5414d132679840b1cd3bd4f2633d79184
Elia Pasqualini <elia.pasqualini@studio.unibo.it>
2021-02-05 20:34:57 +0100

    Create class JTextFieldIO

---
+++ client/src/main/scala/it/amongsus/view/swingio/JTextFieldIO.scala
@@ -0,0 +1,23 @@
+package it.amongsus.view.swingio
+
+import cats.effect.IO
+
+import java.awt.event.FocusListener
+import javax.swing.JTextField
+
+/**
+ * A class that provides a monadic description of the operations supplied by Swing's [[JTextField]] in the form
+ * of IO monad in a purely functional style.
+ * @param component the JTextField that this class wraps.
+ */
+class JTextFieldIO(override val component: JTextField) extends ComponentIO(component){
+
+  def text: IO[String] = IO {component.getText()}
+  def addFocusListener(l:FocusListener) : IO[Unit] = IO {component.addFocusListener(l)}
+
+}
+
+/** Factory for JTextFieldIO instances*/
+object JTextFieldIO {
+  def apply(): IO[JTextFieldIO] = IO { new JTextFieldIO(new JTextField())}
+}
