f58b92080756236595ceed3a72e481f33d47f2e9
Giovanni <giovanni.poggi2@studio.unibo.it>
2021-02-07 10:44:01 +0100

    Added Package Actor and Created UiActorMessages Class

---
+++ client/src/main/scala/it/amongsus/view/actor/UiActorMessages.scala
@@ -0,0 +1,62 @@
+package it.amongsus.view.actor
+
+object UiActorMessages {
+
+  /**
+   * Initialize Actor
+   */
+  case class Init()
+  /**
+   * Create a Public Lobby view event
+   *
+   * @param username the username of the player
+   * @param playersNumber the number of players in the public lobby
+   */
+  case class PublicGameSubmitViewEvent(username: String, playersNumber: Int)
+
+  /**
+   * Join on a Private Lobby view event
+   *
+   * @param username the username of the player
+   * @param privateCode the private code of the lobby
+   */
+  case class PrivateGameSubmitViewEvent(username: String, privateCode: String)
+
+  /**
+   * Create a Private Lobby view event
+   *
+   * @param username the username of the player
+   * @param playersNumber the number of players in the private lobby
+   */
+  case class CreatePrivateGameSubmitViewEvent(username: String, playersNumber: Int)
+
+  /**
+   * Leave the lobby view event
+   */
+  case class LeaveLobbyViewEvent()
+
+  /**
+   * Retry the connection to server view event
+   */
+  case class RetryServerConnection()
+
+  /**
+   * Notify User that was added to a Lobby
+   */
+  case class UserAddedToLobby()
+
+  /**
+   * Notify User that was added to a Lobby
+   */
+  case class PrivateLobbyCreated(lobbyCode: String)
+
+  /**
+   * Notify User that there is a match to a Lobby
+   */
+  case class MatchFound(gameRoom: String)
+
+  /**
+   * Notify User that there's an Error
+   */
+  case object LobbyErrorOccurred
+}
