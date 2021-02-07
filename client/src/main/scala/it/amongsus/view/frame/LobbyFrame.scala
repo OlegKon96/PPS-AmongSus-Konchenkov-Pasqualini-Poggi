5ca7c704b13302df6dec25fff576e221ef2e7190
Elia Pasqualini <elia.pasqualini@studio.unibo.it>
2021-02-07 23:27:01 +0100

    Create basic LobbyFrame

--- client/src/main/scala/it/amongsus/view/frame/LobbyFrame.scala
+++ client/src/main/scala/it/amongsus/view/frame/LobbyFrame.scala
@@ -3,5 +3,15 @@ package it.amongsus.view.frame
 import cats.effect.IO
 
 trait LobbyFrame {
-  def init(): IO[Unit]
+  def start(): IO[Unit]
+}
+
+object LobbyFrame {
+
+  def apply(menuView: MenuFrame): LobbyFrame = new LobbyFrameImpl(menuView)
+
+  private class LobbyFrameImpl(menuView: MenuFrame) extends LobbyFrame {
+    override def start(): IO[Unit] = ???
+  }
+
 }
