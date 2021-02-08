5f63aa43872ca6be0b66486eb78477efc46e4d85
Giovanni <giovanni.poggi2@studio.unibo.it>
2021-02-08 16:16:00 +0100

    Updated UiActorInfo Class

--- client/src/main/scala/it/amongsus/view/actor/UiActorInfo.scala
+++ client/src/main/scala/it/amongsus/view/actor/UiActorInfo.scala
@@ -1,15 +1,26 @@
 package it.amongsus.view.actor
 
 import akka.actor.ActorRef
+import it.amongsus.view.frame.MenuFrame
 
 trait UiActorInfo {
   /** Reference to server actor */
   def clientRef: Option[ActorRef]
+  /** Reference to server actor */
+  def frame: Option[MenuFrame]
+
+  def prova(): Unit
 }
 
 object UiActorInfo {
-  def apply() : UiActorData = UiActorData(None)
-  def apply(clientRef: Option[ActorRef]) : UiActorData = UiActorData(clientRef)
+  def apply() : UiActorData = UiActorData(None, None)
+  def apply(clientRef: Option[ActorRef], frame: Option[MenuFrame]) : UiActorData = UiActorData(clientRef, frame)
 }
 
-case class UiActorData(override val clientRef: Option[ActorRef]) extends UiActorInfo
+case class UiActorData(override val clientRef: Option[ActorRef],
+                       override val frame: Option[MenuFrame]) extends UiActorInfo{
+
+  override def prova(): Unit = {
+    //frame.get.toLobby() unsafeRunSync
+  }
+}
