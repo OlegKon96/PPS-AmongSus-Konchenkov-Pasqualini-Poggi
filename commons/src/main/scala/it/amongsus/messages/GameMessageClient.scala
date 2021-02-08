025bcf534c5853d2d63f3728a272931394906d86
Oleg Konchenkov <konchenkovoleg@gmail.com>
2021-02-08 15:52:57 +0100

    Update GameMessageClient.scala

--- commons/src/main/scala/it/amongsus/messages/GameMessageClient.scala
+++ commons/src/main/scala/it/amongsus/messages/GameMessageClient.scala
@@ -1,18 +1,32 @@
 package it.amongsus.messages
 
 object GameMessageClient {
-
+  /**
+   * Tells the server that a player is ready to start a game
+   */
   case class PlayerReady()
-
+  /**
+   * Tells the server that a player wants to leave the game
+   */
   case class LeaveGame()
-
+  /**
+   * Tells the gui actor that the game is won
+   */
   case class GameWon()
-
+  /**
+   * Tells the gui actor that the game is lost
+   */
   case class GameLost()
-
+  /**
+   * Tells the gui actor that the a player left
+   */
   case class GameEndedBecousePlayerLeft()
-
+  /**
+   * Tells the gui actor that the his action is illegal
+   */
   case class InvalidPlayerAction()
-
+  /**
+   * Tells the server that a client updated his state
+   */
   case class GameStateUpdated()
 }
