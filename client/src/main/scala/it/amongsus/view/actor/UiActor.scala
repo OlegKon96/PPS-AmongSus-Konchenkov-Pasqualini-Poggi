b7fa42692303c7c41eb3d7ee103ca37bb37f020d
Giovanni <giovanni.poggi2@studio.unibo.it>
2021-02-08 16:12:47 +0100

    Updated UiActor Class

--- client/src/main/scala/it/amongsus/view/actor/UiActor.scala
+++ client/src/main/scala/it/amongsus/view/actor/UiActor.scala
@@ -1,6 +1,7 @@
 package it.amongsus.view.actor
 
 import akka.actor.{Actor, ActorLogging, Props}
+import it.amongsus.messages.GameMessageClient._
 import it.amongsus.messages.LobbyMessagesClient._
 import it.amongsus.view.actor.UiActorMessages._
 
@@ -13,19 +14,34 @@ class UiActor(private val serverResponsesListener: UiActorInfo) extends Actor wi
   override def receive: Receive = defaultBehaviour(serverResponsesListener)
 
   private def defaultBehaviour(state: UiActorInfo): Receive = {
-    case Init() => context become defaultBehaviour(UiActorData(Option(sender)))
+    case Init() => context become defaultBehaviour(UiActorData(Option(sender), None))
+    case InitFrame(frame) =>
+      context become defaultBehaviour(UiActorData(state.clientRef, Option(frame)))
     case PublicGameSubmitViewEvent(username, playersNumber) =>
       state.clientRef.get ! JoinPublicLobbyClient(username, playersNumber)
     case PrivateGameSubmitViewEvent(username, privateCode) =>
       state.clientRef.get ! JoinPrivateLobbyClient(username, privateCode)
     case CreatePrivateGameSubmitViewEvent(username, playersNumber) =>
       state.clientRef.get ! CreatePrivateLobbyClient(username, playersNumber)
-    case LeaveLobbyViewEvent => state.clientRef.get ! LeaveLobbyClient()
-    case RetryServerConnection => ???
-    case UserAddedToLobby => ???
+    case LeaveLobbyViewEvent() => state.clientRef.get ! LeaveLobbyClient()
+    case RetryServerConnection() => ???
+    case UserAddedToLobby() => state.prova()
     case PrivateLobbyCreated(lobbyCode) => ???
-    case MatchFound(gameRoom) => ???
+    case GameFound =>
+      state.clientRef.get ! PlayerReady()
+      context become gameBehaviour(state)
     case LobbyErrorOccurred => ???
+    case _ => println("ERROR")
+  }
+
+  private def gameBehaviour(state: UiActorInfo): Receive = {
+    case PlayerReady() => state.clientRef.get ! PlayerReady()
+    case LeaveGame() => state.clientRef.get ! LeaveGame()
+    case GameWon() => ???
+    case GameLost() => ???
+    case GameEndedBecousePlayerLeft() => ???
+    case InvalidPlayerAction() => ???
+    case GameStateUpdated() => ???
     case _ => println("ERROR")
   }
 }
