d84ac3582fbccd7479c26a6d39a2afb5a32da748
Oleg Konchenkov <konchenkovoleg@gmail.com>
2021-02-09 16:58:34 +0100

    Update GameStartupEvent.scala

--- client/src/main/scala/it/amongsus/model/GameStartupEvent.scala
+++ client/src/main/scala/it/amongsus/model/GameStartupEvent.scala
@@ -2,8 +2,4 @@ package it.amongsus.model
 
 sealed class GameStartupEvent
 
-case object LobbyJoinedEvent extends GameStartupEvent
-
-case class PrivateLobbyCreatedEvent(privateCode: String) extends GameStartupEvent
-case class GameStartedEvent() extends GameStartupEvent
 case class LobbyJoinErrorEvent(error: ErrorEvent) extends GameStartupEvent
