a96608b36830ef6aa430cb6a4928d43c6e978607
Oleg Konchenkov <konchenkovoleg@gmail.com>
2021-02-09 17:40:11 +0100

    Update UiActor.scala

--- client/src/main/scala/it/amongsus/view/actor/UiActor.scala
+++ client/src/main/scala/it/amongsus/view/actor/UiActor.scala
@@ -17,33 +17,51 @@ class UiActor(private val serverResponsesListener: UiActorInfo) extends Actor wi
 
   private def defaultBehaviour(state: UiActorInfo): Receive = {
     case Init() => context become defaultBehaviour(UiActorData(Option(sender), None))
+
     case InitFrame(frame) =>
       context become defaultBehaviour(UiActorData(state.clientRef, Option(frame)))
+
     case PublicGameSubmitUi(username, playersNumber) =>
       state.clientRef.get ! JoinPublicLobbyClient(username, playersNumber)
+
     case PrivateGameSubmitUi(username, privateCode) =>
       state.clientRef.get ! JoinPrivateLobbyClient(username, privateCode)
+
     case CreatePrivateGameSubmitUi(username, playersNumber) =>
       state.clientRef.get ! CreatePrivateLobbyClient(username, playersNumber)
+
     case LeaveLobbyUi() => state.clientRef.get ! LeaveLobbyClient()
+
     case RetryServerConnectionUi() => ???
+
     case UserAddedToLobbyUi() => state.prova()
+
     case PrivateLobbyCreatedUi(lobbyCode) => ???
+
     case GameFoundUi() =>
       state.clientRef.get ! PlayerReadyClient()
       context become gameBehaviour(state)
+
     case LobbyErrorOccurredUi => ???
+
     case _ => println("ERROR")
   }
 
   private def gameBehaviour(state: UiActorInfo): Receive = {
     case PlayerReadyUi() => state.clientRef.get ! PlayerReadyClient()
+
     case LeaveGameUi() => state.clientRef.get ! LeaveGameClient()
+
     case GameWonUi() => ???
+
     case GameLostUi() => ???
+
     case PlayerLeftUi() => ???
+
     case InvalidPlayerActionUi() => ???
+
     case GameStateUpdatedUi() => ???
+      
     case _ => println("ERROR")
   }
 }
