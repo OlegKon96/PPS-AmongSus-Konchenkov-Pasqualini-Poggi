0473a85dea39767507d6eb5a8fdf9b7657f6efd1
Elia Pasqualini <elia.pasqualini@studio.unibo.it>
2021-02-10 16:17:11 +0100

    Create basic GameFrame

---
+++ client/src/main/scala/it/amongsus/view/frame/GameFrame.scala
@@ -0,0 +1,19 @@
+package it.amongsus.view.frame
+
+import akka.actor.ActorRef
+import cats.effect.IO
+
+trait GameFrame {
+  def start(): IO[Unit]
+}
+
+object GameFrame {
+
+  def apply(guiRef: Option[ActorRef],menuFrame: MenuFrame): GameFrame = new GameFrameImpl(guiRef,menuFrame)
+
+  private class GameFrameImpl(guiRef: Option[ActorRef],menuFrame: MenuFrame) extends GameFrame {
+
+    override def start(): IO[Unit] = ???
+  }
+
+}
