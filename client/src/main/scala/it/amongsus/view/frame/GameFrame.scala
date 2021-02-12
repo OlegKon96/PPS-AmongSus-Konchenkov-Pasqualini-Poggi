98f48b1cfd5b3c3a2026ae713ea4a098e0118f8b
Elia Pasqualini <elia.pasqualini@studio.unibo.it>
2021-02-12 10:00:02 +0100

    Update start in GameFrame

--- client/src/main/scala/it/amongsus/view/frame/GameFrame.scala
+++ client/src/main/scala/it/amongsus/view/frame/GameFrame.scala
@@ -2,9 +2,14 @@ package it.amongsus.view.frame
 
 import akka.actor.ActorRef
 import cats.effect.IO
+import it.amongsus.view.swingio.JFrameIO
+
+import javax.swing.JFrame
 
 trait GameFrame {
   def start(): IO[Unit]
+
+  def toMenu: IO[Unit]
 }
 
 object GameFrame {
@@ -12,8 +17,18 @@ object GameFrame {
   def apply(guiRef: Option[ActorRef],menuFrame: MenuFrame): GameFrame = new GameFrameImpl(guiRef,menuFrame)
 
   private class GameFrameImpl(guiRef: Option[ActorRef],menuFrame: MenuFrame) extends GameFrame {
+    val gameFrame = new JFrameIO(new JFrame("Among Sus"))
 
-    override def start(): IO[Unit] = ???
+    override def start(): IO[Unit] = for {
+      _ <- gameFrame.setSize(500, 500)
+      _ <- gameFrame.setResizable(false)
+      _ <- gameFrame.setVisible(true)
+    } yield ()
+
+    override def toMenu: IO[Unit] = for{
+      _ <- gameFrame.dispose()
+      _ <- menuFrame start
+    } yield()
   }
 
 }
