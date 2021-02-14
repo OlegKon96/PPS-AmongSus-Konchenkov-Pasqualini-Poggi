adcfcf8d9ffd9351c60da97909534c540708fce2
Elia Pasqualini <elia.pasqualini@studio.unibo.it>
2021-02-14 20:22:16 +0100

    Modify GameFrame

--- client/src/main/scala/it/amongsus/view/frame/GameFrame.scala
+++ client/src/main/scala/it/amongsus/view/frame/GameFrame.scala
@@ -9,31 +9,29 @@ import javax.swing.JFrame
 /**
  *
  */
-trait GameFrame {
+trait GameFrame extends Frame {
   /**
    *
    * @return
    */
   def start(): IO[Unit]
-
-  def toMenu: IO[Unit]
 }
 
 object GameFrame {
-  def apply(guiRef: Option[ActorRef],menuFrame: MenuFrame): GameFrame = new GameFrameImpl(guiRef, menuFrame)
+  def apply(guiRef: Option[ActorRef]): GameFrame = new GameFrameImpl(guiRef)
 
-  private class GameFrameImpl(guiRef: Option[ActorRef],menuFrame: MenuFrame) extends GameFrame {
+  private class GameFrameImpl(guiRef: Option[ActorRef]) extends GameFrame {
     val gameFrame = new JFrameIO(new JFrame("Among Sus"))
+    val WIDTH: Int = 500
+    val HEIGHT: Int = 500
 
     override def start(): IO[Unit] = for {
-      _ <- gameFrame.setSize(500, 500)
+      _ <- gameFrame.setSize(WIDTH, HEIGHT)
       _ <- gameFrame.setResizable(false)
       _ <- gameFrame.setVisible(true)
     } yield ()
 
-    override def toMenu: IO[Unit] = for {
-      _ <- gameFrame.dispose()
-      _ <- menuFrame start
-    } yield ()
+    override def dispose(): IO[Unit] = gameFrame.dispose()
   }
+
 }
