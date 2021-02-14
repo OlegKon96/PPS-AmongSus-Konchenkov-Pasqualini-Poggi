3feab0ef16b7e2dae5a2ea94bdee68d309a32fe7
Oleg Konchenkov <konchenkovoleg@gmail.com>
2021-02-14 12:01:32 +0100

    Update Wall.scala

--- client/src/main/scala/it/amongsus/model/entities/map/Wall.scala
+++ client/src/main/scala/it/amongsus/model/entities/map/Wall.scala
@@ -1,10 +1,12 @@
 package it.amongsus.model.entities.map
 
+import it.amongsus.model.entities.util.Point2D
+
 trait Wall extends Tile {}
 
 object Wall{
-  def apply(/*position: Point2D*/): Wall = WallImpl(/*position*/)
+  def apply(position: Point2D): Wall = WallImpl(position)
 }
 
-case class WallImpl(/*override val position: Point2D*/) extends Wall {
+case class WallImpl(override val position: Point2D) extends Wall {
 }
