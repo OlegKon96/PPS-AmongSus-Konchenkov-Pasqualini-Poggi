22fcd7372eb2f6341eb8f42abd9cf2d5ef50ef70
Oleg Konchenkov <konchenkovoleg@gmail.com>
2021-02-14 12:00:29 +0100

    Update EmergencyButton.scala

--- client/src/main/scala/it/amongsus/model/entities/map/EmergencyButton.scala
+++ client/src/main/scala/it/amongsus/model/entities/map/EmergencyButton.scala
@@ -1,13 +1,15 @@
 package it.amongsus.model.entities.map
 
+import it.amongsus.model.entities.util.Point2D
+
 trait EmergencyButton extends Wall{
   def callEmergency() : Unit
 }
 
 object EmergencyButton{
-  def apply(/*position: Point2D*/): EmergencyButton = EmergencyButtonImpl(/*position*/)
+  def apply(position: Point2D): EmergencyButton = EmergencyButtonImpl(position)
 }
 
-case class EmergencyButtonImpl(/*override val position: Point2D*/) extends EmergencyButton {
+case class EmergencyButtonImpl(override val position: Point2D) extends EmergencyButton {
   override def callEmergency(): Unit = ???
 }
