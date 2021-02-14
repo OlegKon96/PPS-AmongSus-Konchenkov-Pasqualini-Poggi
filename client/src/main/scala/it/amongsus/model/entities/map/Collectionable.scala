8028c59dce30649aabf91d35901025e553df9bd5
Oleg Konchenkov <konchenkovoleg@gmail.com>
2021-02-14 11:58:55 +0100

    Update Collectionable.scala

--- client/src/main/scala/it/amongsus/model/entities/map/Collectionable.scala
+++ client/src/main/scala/it/amongsus/model/entities/map/Collectionable.scala
@@ -1,13 +1,16 @@
 package it.amongsus.model.entities.map
 
-trait Collectionable /*extends Entity*/{
+import it.amongsus.model.entities.Entity
+import it.amongsus.model.entities.util.Point2D
+
+trait Collectionable extends Entity{
   def collect(): Unit
 }
 
 object Collectionable{
-  def apply(/*position: Point2D*/): Collectionable = CollectionableImpl(/*position*/)
+  def apply(position: Point2D): Collectionable = CollectionableImpl(position)
 }
 
-case class CollectionableImpl(/*override val position: Point2D*/) extends Collectionable {
+case class CollectionableImpl(override val position: Point2D) extends Collectionable {
   override def collect(): Unit = ???
 }
