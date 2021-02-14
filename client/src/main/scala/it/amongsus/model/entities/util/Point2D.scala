a7834ffdda69772dccf92bddcb30178a69e24606
Oleg Konchenkov <konchenkovoleg@gmail.com>
2021-02-14 11:58:11 +0100

    Create Point2D.scala

---
+++ client/src/main/scala/it/amongsus/model/entities/util/Point2D.scala
@@ -0,0 +1,13 @@
+package it.amongsus.model.entities.util
+
+trait Point2D{
+  def x: Int
+  def y: Int
+}
+
+object Point2D{
+  def apply(x: Int, y: Int): Point2D = Point2DImpl(x,y)
+}
+
+case class Point2DImpl(override val x: Int,
+                   override val y : Int) extends Point2D
