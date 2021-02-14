973df9f85ffbfd588f9839713bb3a7df65bb04ed
Oleg Konchenkov <konchenkovoleg@gmail.com>
2021-02-14 12:03:46 +0100

    Create Crewmate.scala

---
+++ client/src/main/scala/it/amongsus/model/entities/player/Crewmate.scala
@@ -0,0 +1,24 @@
+package it.amongsus.model.entities.player
+
+import it.amongsus.model.entities.map.Collectionable
+import it.amongsus.model.entities.util.Point2D
+
+trait Crewmate extends AlivePlayer{}
+
+object Crewmate{
+  def apply(position: Point2D): Crewmate = CrewmateImpl(position)
+}
+
+case class CrewmateImpl(override val position: Point2D) extends Crewmate{
+
+  override def move(direction: Any): Unit = ???
+
+  override def vote(): Unit = ???
+
+  override def report(): Unit = ???
+
+  override def callEmergency(): Unit = ???
+
+  private def collect(): Unit = Collectionable.apply(position)
+
+}
