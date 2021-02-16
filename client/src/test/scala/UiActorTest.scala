f7421945839a1f01e051d316d763bcc5b2b48554
Giovanni <giovanni.poggi2@studio.unibo.it>
2021-02-16 14:16:28 +0100

    Updated uiActorTest Class

--- client/src/test/scala/UiActorTest.scala
+++ client/src/test/scala/UiActorTest.scala
@@ -1,3 +1,12 @@
-class UiActorTest {
+import akka.actor.ActorSystem
+import akka.testkit.{ImplicitSender, TestKit}
+import com.typesafe.config.ConfigFactory
+import org.scalatest.BeforeAndAfterAll
+import org.scalatest.wordspec.AnyWordSpecLike
+
+class UiActorTest extends TestKit(ActorSystem("test", ConfigFactory.load("test")))
+  with ImplicitSender
+  with AnyWordSpecLike
+  with BeforeAndAfterAll {
 
 }
