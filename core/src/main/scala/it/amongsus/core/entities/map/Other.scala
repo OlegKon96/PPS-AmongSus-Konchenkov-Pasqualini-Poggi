6cdbb738ec1788e8346c277f411ff259aae626fd
Elia Pasqualini <elia.pasqualini@studio.unibo.it>
2021-02-16 18:54:25 +0100

    Create Other

---
+++ core/src/main/scala/it/amongsus/core/entities/map/Other.scala
@@ -0,0 +1,13 @@
+package it.amongsus.core.entities.map
+
+import it.amongsus.core.entities.util.Point2D
+
+trait Other extends Tile
+
+object Other{
+  def apply(position: Point2D): Other = OtherImpl(position)
+}
+
+case class OtherImpl(override val position: Point2D) extends Other {
+
+}
