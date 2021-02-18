4978917d877d00f57828d9850afa634e2791c2f8
Oleg Konchenkov <konchenkovoleg@gmail.com>
2021-02-18 19:48:52 +0100

    Update Point2D.scala

--- core/src/main/scala/it/amongsus/core/entities/util/Point2D.scala
+++ core/src/main/scala/it/amongsus/core/entities/util/Point2D.scala
@@ -1,13 +1,22 @@
 package it.amongsus.core.entities.util
 
-trait Point2D{
+trait Point2D {
   def x: Int
+
   def y: Int
+
+  def distance(point: Point2D): Int
 }
 
-object Point2D{
-  def apply(x: Int, y: Int): Point2D = Point2DImpl(x,y)
+object Point2D {
+  def apply(x: Int, y: Int): Point2D = Point2DImpl(x, y)
+
+  private case class Point2DImpl(override val x: Int,
+                                 override val y: Int) extends Point2D {
+
+    override def distance(point: Point2D): Int =
+      scala.math.sqrt((point.x - this.x) * (point.x - this.x) + (point.y - this.y) * (point.y - this.y)).toInt
+  }
+
 }
 
-case class Point2DImpl(override val x: Int,
-                   override val y : Int) extends Point2D
