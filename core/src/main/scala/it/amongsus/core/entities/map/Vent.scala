3d3c21ed0e2c9ca624c3e8d74265dd6004827960
Giovanni <giovanni.poggi2@studio.unibo.it>
2021-02-18 16:50:33 +0100

    Update Vent.scala

--- core/src/main/scala/it/amongsus/core/entities/map/Vent.scala
+++ core/src/main/scala/it/amongsus/core/entities/map/Vent.scala
@@ -2,14 +2,17 @@ package it.amongsus.core.entities.map
 
 import it.amongsus.core.entities.util.Point2D
 
+/**
+ * Trait that manages the vent of the game
+ */
 trait Vent extends Floor{
   def useVent(): Unit
 }
 
 object Vent{
   def apply(position: Point2D): Vent = VentImpl(position)
-}
 
-case class VentImpl(override val position: Point2D) extends Vent {
-  override def useVent(): Unit = ???
+  private case class VentImpl(override val position: Point2D) extends Vent {
+    override def useVent(): Unit = ???
+  }
 }
