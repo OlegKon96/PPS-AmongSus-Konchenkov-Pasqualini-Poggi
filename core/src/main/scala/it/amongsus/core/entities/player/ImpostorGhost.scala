0c610caac76e0a340da8b100af563d166d40ed5b
Elia Pasqualini <elia.pasqualini@studio.unibo.it>
2021-02-18 22:20:30 +0100

    Update ImpostorGhost

--- core/src/main/scala/it/amongsus/core/entities/player/ImpostorGhost.scala
+++ core/src/main/scala/it/amongsus/core/entities/player/ImpostorGhost.scala
@@ -1,36 +1,33 @@
 package it.amongsus.core.entities.player
 
-import java.awt.Color
-
-import it.amongsus.core.entities.player.Movement._
+import it.amongsus.core.entities.map.Tile
+import it.amongsus.core.entities.util.Movement.{Down, Left, Right, Up}
 import it.amongsus.core.entities.util.Point2D
+
 
 /**
  * Trait that manages the Ghost of the Impostor of the game
  */
-trait ImpostorGhost extends DeadPlayer{
-  /**
-   * Manages the Impostor that can sabotage an item of the game
-   */
-  def sabotage():Unit
-}
+trait ImpostorGhost extends DeadPlayer with Impostor
 
 object ImpostorGhost{
   def apply(clientId: String, username: String, position: Point2D): ImpostorGhost =
-    ImpostorGhostImpl("green", clientId, username, position)
-}
+    ImpostorGhostImpl("green", clientId, username, Constants.Impostor.FIELD_OF_VIEW, position)
 
-case class ImpostorGhostImpl(override val color: String,
-                             override val clientId: String,
-                             override val username: String,
-                             override val position: Point2D) extends ImpostorGhost {
+  private case class ImpostorGhostImpl(override val color: String,
+                                       override val clientId: String,
+                                       override val username: String,
+                                       override val fieldOfView: Int,
+                                       override val position: Point2D) extends ImpostorGhost {
 
-  override def move(direction: Movement): Unit = direction match{
-    case Up() => ImpostorGhost(clientId, username, Point2D(position.x, position.y + 1))
-    case Down() => ImpostorGhost(clientId, username, Point2D(position.x, position.y - 1))
-    case Left() => ImpostorGhost(clientId, username, Point2D(position.x - 1, position.y))
-    case Right() => ImpostorGhost(clientId,username, Point2D(position.x + 1, position.y))
+    override def move(direction: Movement, map: Array[Array[Tile]]): Option[Player] = {
+      val newPlayer = direction match {
+        case Up() => ImpostorGhost(clientId, username, Point2D(position.x - 1, position.y))
+        case Down() => ImpostorGhost(clientId,username, Point2D(position.x + 1, position.y))
+        case Left() => ImpostorGhost(clientId, username, Point2D(position.x, position.y - 1))
+        case Right() => ImpostorGhost(clientId, username, Point2D(position.x, position.y + 1))
+      }
+      if (checkCollision(newPlayer.position, map)) None else Option(newPlayer)
+    }
   }
-
-  override def sabotage(): Unit = ???
 }
