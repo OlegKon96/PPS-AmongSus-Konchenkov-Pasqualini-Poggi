013a4aa503b71b5a28d173adfb77c3b21337a885
Oleg Konchenkov <konchenkovoleg@gmail.com>
2021-02-18 19:48:56 +0100

    Update Player.scala

--- core/src/main/scala/it/amongsus/core/entities/player/Player.scala
+++ core/src/main/scala/it/amongsus/core/entities/player/Player.scala
@@ -1,13 +1,15 @@
 package it.amongsus.core.entities.player
 
 import it.amongsus.core.entities.Entity
-import it.amongsus.core.entities.util.Point2D
+import it.amongsus.core.entities.map.Tile
+import it.amongsus.core.entities.util.Movement
 
 /**
  * Trait that represents the actions of the Game's Player
  */
 trait Player extends Entity{
-  def move(direction: Movement) : Unit
+  def move(direction: Movement, map: Array[Array[Tile]]) : Option[Player]
   def username: String
   def clientId: String
+  def fieldOfView: Int
 }
