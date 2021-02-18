b1c73301e6b17ff8c48f2e4050342662cccc7183
Giovanni <giovanni.poggi2@studio.unibo.it>
2021-02-18 16:42:21 +0100

    Update Boundary.scala

--- core/src/main/scala/it/amongsus/core/entities/map/Boundary.scala
+++ core/src/main/scala/it/amongsus/core/entities/map/Boundary.scala
@@ -1,13 +1,12 @@
 package it.amongsus.core.entities.map
 
+
 import it.amongsus.core.entities.util.Point2D
 
 trait Boundary extends Wall
 
 object Boundary{
   def apply(position: Point2D): Boundary = BoundaryImpl(position)
-}
 
-case class BoundaryImpl(override val position: Point2D) extends Boundary {
-
+  private case class BoundaryImpl(override val position: Point2D) extends Boundary
 }
