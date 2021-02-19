f02dfef4f01c1500d1cc1af63c96d96e4e40bc7c
Elia Pasqualini <elia.pasqualini@studio.unibo.it>
2021-02-19 20:54:20 +0100

    Update JFrameIO

--- client/src/main/scala/it/amongsus/view/swingio/JFrameIO.scala
+++ client/src/main/scala/it/amongsus/view/swingio/JFrameIO.scala
@@ -1,6 +1,8 @@
 package it.amongsus.view.swingio
 
 import cats.effect.IO
+
+import java.awt.event.KeyListener
 import java.awt.{Component, Container, Frame}
 import javax.swing.JFrame
 
@@ -17,6 +19,7 @@ class JFrameIO(override val component: JFrame) extends ContainerIO(component) {
   def dispose() : IO[Unit] = IO{component.dispose()}
   def setVisible(b: Boolean): IO[Unit] = IO(component.setVisible(b))
   def setResizable(resizable: Boolean): IO[Unit] = IO(component.setResizable(resizable))
+  def addKeyListener(k : KeyListener) : IO[Unit] = IO(component.addKeyListener(k))
 
   //invoke and wait versions (for finer granularity for task assignment to EDT thread)
   def setResizableInvokingAndWaiting(resizable: Boolean): IO[Unit] = invokeAndWaitIO(component.setResizable(resizable))
