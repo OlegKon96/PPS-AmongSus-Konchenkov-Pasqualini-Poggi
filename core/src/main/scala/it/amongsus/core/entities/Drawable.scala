6032cb4e4cea8a445680de7975eebe3b3c13dee4
Giovanni <giovanni.poggi2@studio.unibo.it>
2021-02-20 21:51:31 +0100

    Update Drawable.scala

--- core/src/main/scala/it/amongsus/core/entities/Drawable.scala
+++ core/src/main/scala/it/amongsus/core/entities/Drawable.scala
@@ -3,5 +3,10 @@ package it.amongsus.core.entities
 import it.amongsus.core.entities.util.Point2D
 
 trait Drawable {
+  /**
+   * Position of the drawable
+   *
+   * @return
+   */
   def position: Point2D
 }
