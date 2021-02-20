01b4e929e1723a256d522f59e6e483dd8efcf3ed
Giovanni <giovanni.poggi2@studio.unibo.it>
2021-02-20 21:48:01 +0100

    Update Impostor.scala

--- core/src/main/scala/it/amongsus/core/entities/player/Impostor.scala
+++ core/src/main/scala/it/amongsus/core/entities/player/Impostor.scala
@@ -1,12 +1,11 @@
 package it.amongsus.core.entities.player
 
 /**
- * Trait that manages the Impostor of the game
+ * Trait that manages the Impostor
  */
 trait Impostor extends Player{
   /**
    * Manages the sabotage of the game
    */
-  def sabotage(): Unit = ???
-
+  def sabotage(): Unit = {}
 }
