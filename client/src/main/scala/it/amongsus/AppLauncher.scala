cce86e7f67958a09157f569b360089a29608ffa0
Giovanni <giovanni.poggi2@studio.unibo.it>
2021-02-20 21:41:12 +0100

    Update AppLauncher.scala

--- client/src/main/scala/it/amongsus/AppLauncher.scala
+++ client/src/main/scala/it/amongsus/AppLauncher.scala
@@ -3,8 +3,8 @@ package it.amongsus
 import it.amongsus.controller.{MainController, MainControllerImpl}
 
 /**
-  * Class that initializes the whole client application
-  */
+ * Class that initializes the whole client application
+ */
 object AppLauncher extends App {
   val controller: MainController = new MainControllerImpl()
   controller.start()
