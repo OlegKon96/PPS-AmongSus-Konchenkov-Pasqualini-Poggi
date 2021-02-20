13eb3329f0fd858b3bd21364e5cc0ddf4ebfd615
Giovanni <giovanni.poggi2@studio.unibo.it>
2021-02-20 21:35:53 +0100

    Update MainControllerImpl.scala

--- client/src/main/scala/it/amongsus/controller/MainControllerImpl.scala
+++ client/src/main/scala/it/amongsus/controller/MainControllerImpl.scala
@@ -1,8 +1,8 @@
 package it.amongsus.controller
 
-import it.amongsus.controller.actor.{ControllerActor, LobbyActorInfo}
 import it.amongsus.{ActorSystemManager, Constants}
 import it.amongsus.messages.LobbyMessagesClient.ConnectClient
+import it.amongsus.controller.actor.{ControllerActor, LobbyActorInfo}
 import it.amongsus.view.actor.{UiActor, UiActorInfo}
 
 /**
@@ -13,8 +13,13 @@ class MainControllerImpl() extends MainController {
   private lazy val guiRef =
     ActorSystemManager.actorSystem.actorOf(UiActor.props(UiActorInfo()), "gui")
   private lazy val lobbyActorRef =
-    ActorSystemManager.actorSystem.actorOf(ControllerActor.props(LobbyActorInfo(Option(guiRef))), "client-controller")
+    ActorSystemManager.actorSystem.actorOf(ControllerActor.props(LobbyActorInfo(Option(guiRef))), "client-lobby")
 
-  override def start(): Unit =
+  override def start(): Unit = {
+    connect()
+  }
+
+  private def connect(): Unit = {
     this.lobbyActorRef.tell(ConnectClient(Constants.Remote.SERVER_ADDRESS, Constants.Remote.SERVER_PORT), lobbyActorRef)
+  }
 }
