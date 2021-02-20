a940288f46bb6b546a41cb96a722a07f2ffd1cd6
Oleg Konchenkov <konchenkovoleg@gmail.com>
2021-02-20 17:56:32 +0100

    Update CrewmateAlive adn CrewmateGhost

--- core/src/main/scala/it/amongsus/core/entities/player/CrewmateAlive.scala
+++ core/src/main/scala/it/amongsus/core/entities/player/CrewmateAlive.scala
@@ -4,28 +4,30 @@ import it.amongsus.core.entities.map.Tile
 import it.amongsus.core.entities.util.Movement._
 import it.amongsus.core.entities.util.{Movement, Point2D}
 
+/**
+ * Trait that manages the Crewmate Alive
+ */
 trait CrewmateAlive extends AlivePlayer with Crewmate
 
 object CrewmateAlive {
-  def apply(clientId: String, username: String, position: Point2D): CrewmateAlive =
+  def apply(clientId: String, username: String, numCoins: Int, position: Point2D): CrewmateAlive =
     CrewmateAliveImpl("green", clientId, username, Constants.Crewmate.FIELD_OF_VIEW,
-      Constants.Crewmate.NUM_COINS, position)
+      numCoins, position)
 
   private case class CrewmateAliveImpl(override val color: String,
                                        override val clientId: String,
                                        override val username: String,
                                        override val fieldOfView: Int,
-                                       override var numCoins: Int,
+                                       override val numCoins: Int,
                                        override val position: Point2D) extends CrewmateAlive {
 
     override def move(direction: Movement, map: Array[Array[Tile]]): Option[Player] = {
       val newPlayer = direction match {
-        case Up() => CrewmateAlive(clientId, username, Point2D(position.x - 1, position.y))
-        case Down() => CrewmateAlive(clientId,username, Point2D(position.x + 1, position.y))
-        case Left() => CrewmateAlive(clientId, username, Point2D(position.x, position.y - 1))
-        case Right() => CrewmateAlive(clientId, username, Point2D(position.x, position.y + 1))
+        case Up() => CrewmateAlive(clientId, username, numCoins, Point2D(position.x - 1, position.y))
+        case Down() => CrewmateAlive(clientId,username, numCoins, Point2D(position.x + 1, position.y))
+        case Left() => CrewmateAlive(clientId, username, numCoins, Point2D(position.x, position.y - 1))
+        case Right() => CrewmateAlive(clientId, username, numCoins, Point2D(position.x, position.y + 1))
       }
-
       if (checkCollision(newPlayer.position, map)) None else Option(newPlayer)
     }
   }
--- core/src/main/scala/it/amongsus/core/entities/player/CrewmateGhost.scala
+++ core/src/main/scala/it/amongsus/core/entities/player/CrewmateGhost.scala
@@ -4,31 +4,31 @@ import it.amongsus.core.entities.map.Tile
 import it.amongsus.core.entities.util.Movement._
 import it.amongsus.core.entities.util.{Movement, Point2D}
 
+/**
+ * Method to manages the Crewmate ghost
+ */
 trait CrewmateGhost extends DeadPlayer with Crewmate
 
 object CrewmateGhost {
-  def apply(clientId: String, username: String, position: Point2D): CrewmateGhost =
+  def apply(clientId: String, username: String, numCoins: Int, position: Point2D): CrewmateGhost =
     CrewmateGhostImpl("green", clientId, username, Constants.Crewmate.FIELD_OF_VIEW,
-      Constants.Crewmate.NUM_COINS, position)
+      numCoins, position)
 
   private case class CrewmateGhostImpl(override val color: String,
                                        override val clientId: String,
                                        override val username: String,
                                        override val fieldOfView: Int,
-                                       override var numCoins: Int,
+                                       override val numCoins: Int,
                                        override val position: Point2D) extends CrewmateGhost {
 
     override def move(direction: Movement, map: Array[Array[Tile]]): Option[Player] = {
       val newPlayer = direction match {
-        case Up() => CrewmateGhost(clientId, username, Point2D(position.x - 1, position.y))
-        case Down() => CrewmateGhost(clientId, username, Point2D(position.x + 1, position.y))
-        case Left() => CrewmateGhost(clientId, username, Point2D(position.x, position.y - 1))
-        case Right() => CrewmateGhost(clientId, username, Point2D(position.x, position.y + 1))
+        case Up() => CrewmateGhost(clientId, username, numCoins, Point2D(position.x - 1, position.y))
+        case Down() => CrewmateGhost(clientId, username, numCoins, Point2D(position.x + 1, position.y))
+        case Left() => CrewmateGhost(clientId, username, numCoins, Point2D(position.x, position.y - 1))
+        case Right() => CrewmateGhost(clientId, username, numCoins, Point2D(position.x, position.y + 1))
       }
-
       if (checkCollision(newPlayer.position, map)) None else Option(newPlayer)
     }
   }
-
 }
-
