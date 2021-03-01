c872eb1095904a2984f4a5f1dd2fe58bb7d9b7ae
Giovanni <giovanni.poggi2@studio.unibo.it>
2021-03-01 18:08:20 +0100

    Update JScrollPaneIO.scala
    Added Monadic JScrollPaneIO to the scala class

--- client/src/main/scala/it/amongsus/view/swingio/JScrollPaneIO.scala
+++ client/src/main/scala/it/amongsus/view/swingio/JScrollPaneIO.scala
@@ -1,5 +1,19 @@
 package it.amongsus.view.swingio
 
-class JScrollPaneIO {
+import cats.effect.IO
+import javax.swing.{JScrollPane, JTextArea}
 
+/**
+ * A class that provides a monadic description of the operations supplied by Swing's [[JTextArea]] in the form
+ * of IO monad in a purely functional style.
+ *
+ * @param component the JTextArea that this class wraps.
+ */
+class JScrollPaneIO(override val component: JScrollPane) extends ComponentIO(component){}
+
+/**
+ * Factory for JTextAreaIO instances
+ */
+object JScrollPaneIO {
+  def apply(textArea: JTextAreaIO): IO[JScrollPaneIO] = IO { new JScrollPaneIO(new JScrollPane(textArea.component)) }
 }
