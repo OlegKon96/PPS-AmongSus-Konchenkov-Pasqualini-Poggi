9ae3df5bcd5af5a8e0133b851e01acf04d2b03f9
Giovanni <giovanni.poggi2@studio.unibo.it>
2021-03-02 14:03:27 +0100

    Update Message.scala
    Implemented Message.scala

--- core/src/main/scala/it/amongsus/core/entities/util/Message.scala
+++ core/src/main/scala/it/amongsus/core/entities/util/Message.scala
@@ -1,5 +1,12 @@
 package it.amongsus.core.entities.util
 
-class Message {
+trait Message{
+  def username: String
+  def text: String
+}
 
+object Message {
+  def apply(username: String, text: String) : Message = MessageImpl(username, text)
+
+  private case class MessageImpl(override val username: String, override val text: String) extends Message
 }
