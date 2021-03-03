bc54daf30e7762e13207ad2e4f0ab7af17587fc2
Elia Pasqualini <elia.pasqualini@studio.unibo.it>
2021-03-03 18:51:22 +0100

    Update drawMap in GamePanel, implement dynamic player's view

--- client/src/main/scala/it/amongsus/view/panel/GamePanel.scala
+++ client/src/main/scala/it/amongsus/view/panel/GamePanel.scala
@@ -1,8 +1,9 @@
 package it.amongsus.view.panel
 
-import it.amongsus.core.entities.map.{Boundary, Collectionable, DeadBody, Emergency, Floor, Other, Tile, Vent, Wall}
-import it.amongsus.core.entities.player.{AlivePlayer, CrewmateAlive, CrewmateGhost, DeadPlayer, ImpostorAlive, ImpostorGhost, Player}
-
+import it.amongsus.core.entities.map.{Boundary, Collectionable, DeadBody, Emergency,
+  Floor, Other, Tile, Vent, Wall}
+import it.amongsus.core.entities.player.{AlivePlayer, CrewmateAlive, CrewmateGhost,
+  DeadPlayer, ImpostorAlive, ImpostorGhost, Player}
 import java.awt.Graphics
 import java.awt.image.BufferedImage
 import java.io.File
@@ -14,7 +15,6 @@ trait GamePanel extends JPanel{
                  players: Seq[Player],
                  collectionables : Seq[Collectionable],
                  deadBodies : Seq[DeadBody]) : Unit
-
 }
 
 object GamePanel {
@@ -37,83 +37,136 @@ object GamePanel {
     private var gameMyChar = myChar
 
     val block: BufferedImage = ImageIO.read(new File("res/block.png"))
+    val blockOff: BufferedImage = ImageIO.read(new File("res/blockOff.png"))
     val space: BufferedImage = ImageIO.read(new File("res/space.png"))
     val floor: BufferedImage = ImageIO.read(new File("res/floor.png"))
+    val floorOff: BufferedImage = ImageIO.read(new File("res/floorOff.png"))
     val vent: BufferedImage = ImageIO.read(new File("res/vent.png"))
+    val ventOff : BufferedImage = ImageIO.read(new File("res/ventOff.png"))
     val emergency: BufferedImage = ImageIO.read(new File("res/emergencyButton.png"))
+    val emergencyOff : BufferedImage = ImageIO.read(new File("res/emergencyButtonOff.png"))
     val coin: BufferedImage = ImageIO.read(new File("res/coin.png"))
     val SCALEFACTOR: Int = 15
     val ONE : Int = 1
+    val ZERO : Int = 0
+    val WIDTH : Int = 1080
+    val HEIGHT : Int = 750
 
     override def paintComponent(g: Graphics): Unit = {
-      g.clearRect(0, 0, 1080, 750)
+      g.clearRect(ZERO, ZERO, WIDTH, HEIGHT)
       drawMap(g)
-      drawPlayers(g)
+      drawEntities(g)
     }
 
     private def drawMap(g: Graphics): Unit = {
-      gameMap.foreach(x => x.foreach {
-        case tile: Vent => g.drawImage(vent, tile.position.y * SCALEFACTOR + ONE,
-          tile.position.x * SCALEFACTOR + ONE,
-          SCALEFACTOR, SCALEFACTOR, this)
-        case tile: Emergency => g.drawImage(emergency, tile.position.y * SCALEFACTOR + ONE,
-          tile.position.x * SCALEFACTOR + ONE,
-          SCALEFACTOR, SCALEFACTOR, this)
-        case tile: Boundary => g.drawImage(space, tile.position.y * SCALEFACTOR + ONE,
-          tile.position.x * SCALEFACTOR + ONE,
-          SCALEFACTOR, SCALEFACTOR, this)
-        case tile: Wall => g.drawImage(block, tile.position.y * SCALEFACTOR + ONE,
-          tile.position.x * SCALEFACTOR + ONE,
-          SCALEFACTOR, SCALEFACTOR, this)
-        case tile: Floor => g.drawImage(floor, tile.position.y * SCALEFACTOR + ONE,
-          tile.position.x * SCALEFACTOR + ONE,
-          SCALEFACTOR, SCALEFACTOR, this)
-        case tile: Other => g.drawImage(space, tile.position.y * SCALEFACTOR + ONE,
-          tile.position.x * SCALEFACTOR + ONE,
-          SCALEFACTOR, SCALEFACTOR, this)
-      })
+      gameMyChar match {
+        case _: AlivePlayer =>
+          gameMap.foreach(x => x.foreach {
+            case tile: Vent => if (tile.position.distance(gameMyChar.position) < gameMyChar.fieldOfView) {
+                g.drawImage(vent,
+                  tile.position.y * SCALEFACTOR + ONE,
+                  tile.position.x * SCALEFACTOR + ONE,
+                  SCALEFACTOR, SCALEFACTOR, this)
+              } else { g.drawImage(ventOff,
+                  tile.position.y * SCALEFACTOR + ONE,
+                  tile.position.x * SCALEFACTOR + ONE,
+                  SCALEFACTOR, SCALEFACTOR, this)
+              }
+            case tile: Emergency => if (tile.position.distance(gameMyChar.position) < gameMyChar.fieldOfView) {
+              g.drawImage(emergency,
+                  tile.position.y * SCALEFACTOR + ONE,
+                  tile.position.x * SCALEFACTOR + ONE,
+                  SCALEFACTOR, SCALEFACTOR, this)
+              } else { g.drawImage(emergencyOff,
+                  tile.position.y * SCALEFACTOR + ONE,
+                  tile.position.x * SCALEFACTOR + ONE,
+                  SCALEFACTOR, SCALEFACTOR, this)
+              }
+            case tile: Boundary => g.drawImage(space,
+                tile.position.y * SCALEFACTOR + ONE,
+                tile.position.x * SCALEFACTOR + ONE,
+                SCALEFACTOR, SCALEFACTOR, this)
+            case tile: Wall =>
+              if (tile.position.distance(gameMyChar.position) < gameMyChar.fieldOfView) {
+                g.drawImage(block,
+                  tile.position.y * SCALEFACTOR + ONE,
+                  tile.position.x * SCALEFACTOR + ONE,
+                  SCALEFACTOR, SCALEFACTOR, this)
+              } else { g.drawImage(blockOff,
+                  tile.position.y * SCALEFACTOR + ONE,
+                  tile.position.x * SCALEFACTOR + ONE,
+                  SCALEFACTOR, SCALEFACTOR, this)
+              }
+            case tile: Floor => if (tile.position.distance(gameMyChar.position) < gameMyChar.fieldOfView) {
+                g.drawImage(floor,
+                  tile.position.y * SCALEFACTOR + ONE,
+                  tile.position.x * SCALEFACTOR + ONE,
+                  SCALEFACTOR, SCALEFACTOR, this)
+              } else { g.drawImage(floorOff,
+                  tile.position.y * SCALEFACTOR + ONE,
+                  tile.position.x * SCALEFACTOR + ONE,
+                  SCALEFACTOR, SCALEFACTOR, this)
+              }
+            case tile: Other => g.drawImage(space,
+                tile.position.y * SCALEFACTOR + ONE,
+                tile.position.x * SCALEFACTOR + ONE,
+                SCALEFACTOR, SCALEFACTOR, this)
+          })
+        case _: DeadPlayer => gameMap.foreach(x => x.foreach {
+            case tile: Vent => g.drawImage(vent,
+              tile.position.y * SCALEFACTOR + ONE,
+              tile.position.x * SCALEFACTOR + ONE,
+                SCALEFACTOR, SCALEFACTOR, this)
+            case tile: Emergency => g.drawImage(emergency,
+              tile.position.y * SCALEFACTOR + ONE,
+              tile.position.x * SCALEFACTOR + ONE,
+              SCALEFACTOR, SCALEFACTOR, this)
+            case tile: Boundary => g.drawImage(space,
+              tile.position.y * SCALEFACTOR + ONE,
+              tile.position.x * SCALEFACTOR + ONE,
+              SCALEFACTOR, SCALEFACTOR, this)
+            case tile: Wall => g.drawImage(block,
+              tile.position.y * SCALEFACTOR + ONE,
+              tile.position.x * SCALEFACTOR + ONE,
+              SCALEFACTOR, SCALEFACTOR, this)
+            case tile: Floor => g.drawImage(floor,
+              tile.position.y * SCALEFACTOR + ONE,
+              tile.position.x * SCALEFACTOR + ONE,
+              SCALEFACTOR, SCALEFACTOR, this)
+            case tile: Other => g.drawImage(space,
+              tile.position.y * SCALEFACTOR + ONE,
+              tile.position.x * SCALEFACTOR + ONE,
+              SCALEFACTOR, SCALEFACTOR, this)
+          })
+      }
     }
 
-    private def drawPlayers(g: Graphics): Unit = {
+    private def drawEntities(g: Graphics): Unit = {
       gameMyChar match {
-        case _: ImpostorAlive => {
+        case _: ImpostorAlive =>
           gameDeadBodies.filter(body => body.position.distance(gameMyChar.position) < gameMyChar.fieldOfView)
-            .foreach(deadBody => g.drawImage(getImageDead(deadBody.color), deadBody.position.y * SCALEFACTOR + ONE,
+            .foreach(deadBody => g.drawImage(getImageDead(deadBody.color),
+              deadBody.position.y * SCALEFACTOR + ONE,
               deadBody.position.x * SCALEFACTOR + ONE,
-              SCALEFACTOR, SCALEFACTOR, null))
+              SCALEFACTOR, SCALEFACTOR, this))
           gamePlayers.filter(player => player.position.distance(gameMyChar.position) < gameMyChar.fieldOfView)
             .foreach {
-            case impostorAlive: ImpostorAlive => {
-              drawImpostor(g, impostorAlive, getImageAlive(impostorAlive.color))
-            }
-            case crewmateAlive: CrewmateAlive => {
-              drawCrewmate(g, crewmateAlive, getImageAlive(crewmateAlive.color))
-            }
+            case impostorAlive: ImpostorAlive => drawImpostor(g, impostorAlive, getImageAlive(impostorAlive.color))
+            case crewmateAlive: CrewmateAlive => drawCrewmate(g, crewmateAlive, getImageAlive(crewmateAlive.color))
             case _ =>
           }
-        }
-        case _: ImpostorGhost => {
+        case _: ImpostorGhost =>
           gameDeadBodies.foreach(deadBody => g.drawImage(getImageDead(deadBody.color),
             deadBody.position.y * SCALEFACTOR + ONE,
             deadBody.position.x * SCALEFACTOR + ONE,
-            SCALEFACTOR, SCALEFACTOR, null))
+            SCALEFACTOR, SCALEFACTOR, this))
           gamePlayers.foreach {
-            case impostorAlive: ImpostorAlive => {
-              drawImpostor(g, impostorAlive, getImageAlive(impostorAlive.color))
-            }
-            case impostorGhost: ImpostorGhost => {
-              drawImpostor(g, impostorGhost, getImageGhost(impostorGhost.color))
-            }
-            case crewmateAlive: CrewmateAlive => {
-              drawCrewmate(g, crewmateAlive, getImageAlive(crewmateAlive.color))
-            }
-            case crewmateGhost: CrewmateGhost => {
-              drawCrewmate(g, crewmateGhost, getImageGhost(crewmateGhost.color))
-            }
+            case impostorAlive: ImpostorAlive => drawImpostor(g, impostorAlive, getImageAlive(impostorAlive.color))
+            case impostorGhost: ImpostorGhost => drawImpostor(g, impostorGhost, getImageGhost(impostorGhost.color))
+            case crewmateAlive: CrewmateAlive => drawCrewmate(g, crewmateAlive, getImageAlive(crewmateAlive.color))
+            case crewmateGhost: CrewmateGhost => drawCrewmate(g, crewmateGhost, getImageGhost(crewmateGhost.color))
           }
-
-        }
-        case _: CrewmateAlive => {
+        case _: CrewmateAlive =>
           gameDeadBodies.filter(body => body.position.distance(gameMyChar.position) < gameMyChar.fieldOfView)
             .foreach(deadBody => g.drawImage(getImageDead(deadBody.color),
               deadBody.position.y * SCALEFACTOR + ONE,
@@ -124,14 +177,13 @@ object GamePanel {
             .foreach(collectionable => g.drawImage(coin,
               collectionable.position.y * SCALEFACTOR + ONE,
               collectionable.position.x * SCALEFACTOR + ONE,
-              SCALEFACTOR, SCALEFACTOR, null))
+              SCALEFACTOR, SCALEFACTOR, this))
           gamePlayers.filter(player => player.position.distance(gameMyChar.position) < gameMyChar.fieldOfView)
             .foreach {
             case alivePlayer: AlivePlayer => drawCrewmate(g, alivePlayer, getImageAlive(alivePlayer.color))
             case _ =>
           }
-        }
-        case _: CrewmateGhost => {
+        case _: CrewmateGhost =>
           gameDeadBodies.foreach(deadBody => g.drawImage(getImageDead(deadBody.color),
             deadBody.position.y * SCALEFACTOR + ONE,
             deadBody.position.x * SCALEFACTOR + ONE,
@@ -141,29 +193,23 @@ object GamePanel {
             collectionable.position.x * SCALEFACTOR + ONE,
             SCALEFACTOR, SCALEFACTOR, this))
           gamePlayers.foreach {
-            case ap: AlivePlayer => {
-              drawCrewmate(g, ap, getImageAlive(ap.color))
-            }
-            case dp: DeadPlayer => {
-              drawCrewmate(g, dp, getImageGhost(dp.color))
-            }
+            case ap: AlivePlayer => drawCrewmate(g, ap, getImageAlive(ap.color))
+            case dp: DeadPlayer => drawCrewmate(g, dp, getImageGhost(dp.color))
           }
-
-        }
 
       }
     }
 
     private def getImageAlive(color: String): BufferedImage = {
-      ImageIO.read(new File(s"res/playerAlive${color}.png"))
+      ImageIO.read(new File(s"res/playerAlive$color.png"))
     }
 
     private def getImageGhost(color: String): BufferedImage = {
-      ImageIO.read(new File(s"res/playerGhost${color}.png"))
+      ImageIO.read(new File(s"res/playerGhost$color.png"))
     }
 
     private def getImageDead(color: String): BufferedImage = {
-      ImageIO.read(new File(s"res/playerDead${color}.png"))
+      ImageIO.read(new File(s"res/playerDead$color.png"))
     }
 
     private def drawImpostor(g: Graphics, player: Player, image: BufferedImage): Unit = {
