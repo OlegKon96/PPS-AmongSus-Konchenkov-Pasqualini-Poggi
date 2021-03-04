51a2a75102ddb71ba2196d5e19466f8a05296bb2
Giovanni <giovanni.poggi2@studio.unibo.it>
2021-03-04 11:51:53 +0100

    Update WinFrame.scala
    Created Object and Implemented WinFrameImpl class

--- client/src/main/scala/it/amongsus/view/frame/WinFrame.scala
+++ client/src/main/scala/it/amongsus/view/frame/WinFrame.scala
@@ -1,9 +1,11 @@
 package it.amongsus.view.frame
 
 import java.awt.BorderLayout
+
 import akka.actor.ActorRef
 import cats.effect.IO
 import it.amongsus.core.entities.player.{Crewmate, Impostor, Player}
+import it.amongsus.view.frame.WinFrame.WinFrameImpl
 import it.amongsus.view.swingio.{BorderFactoryIO, JFrameIO, JLabelIO, JPanelIO}
 import javax.swing.JFrame
 
@@ -17,4 +19,30 @@ trait WinFrame extends Frame {
    * @return
    */
   def start(): IO[Unit]
+}
+
+object WinFrame {
+  def apply(guiRef: Option[ActorRef], winnerCrew: Player): WinFrame =
+    new WinFrameImpl(guiRef: Option[ActorRef], winnerCrew: Player)
+
+  /**
+   * The Frame that manages the winning
+   *
+   * @param guiRef ActorRef that is responsible to receiving and send all the messages about winning
+   */
+  private class WinFrameImpl(guiRef: Option[ActorRef], winnerCrew: Player) extends WinFrame() {
+    /**
+     * Open the Vote Frame to display the winner team
+     *
+     * @return
+     */
+    override def start(): IO[Unit] = ???
+
+    override def dispose(): IO[Unit] = ???
+  }
+
+    override def dispose(): IO[Unit] = for {
+      _ <- frame.dispose()
+    } yield ()
+  }
 }
