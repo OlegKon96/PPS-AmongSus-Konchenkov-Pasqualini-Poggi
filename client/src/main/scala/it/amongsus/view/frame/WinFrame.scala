fe440045cce0a8c143456f670c102b04eeb7ad7b
Giovanni <giovanni.poggi2@studio.unibo.it>
2021-03-04 12:49:47 +0100

    Update WinFrame.scala
    Implemented start on WinFrameImpl

--- client/src/main/scala/it/amongsus/view/frame/WinFrame.scala
+++ client/src/main/scala/it/amongsus/view/frame/WinFrame.scala
@@ -1,11 +1,9 @@
 package it.amongsus.view.frame
 
 import java.awt.BorderLayout
-
 import akka.actor.ActorRef
 import cats.effect.IO
 import it.amongsus.core.entities.player.{Crewmate, Impostor, Player}
-import it.amongsus.view.frame.WinFrame.WinFrameImpl
 import it.amongsus.view.swingio.{BorderFactoryIO, JFrameIO, JLabelIO, JPanelIO}
 import javax.swing.JFrame
 
@@ -31,15 +29,40 @@ object WinFrame {
    * @param guiRef ActorRef that is responsible to receiving and send all the messages about winning
    */
   private class WinFrameImpl(guiRef: Option[ActorRef], winnerCrew: Player) extends WinFrame() {
-    /**
-     * Open the Vote Frame to display the winner team
-     *
-     * @return
-     */
-    override def start(): IO[Unit] = ???
 
-    override def dispose(): IO[Unit] = ???
-  }
+    final val spaceDimension10: Int = 10
+    final val spaceDimension60: Int = 60
+    val frame = new JFrameIO(new JFrame("Among Sus - Winner"))
+    val votePanel: JPanelIO = JPanelIO().unsafeRunSync()
+    val WIDTH: Int = 1000
+    val HEIGHT: Int = 800
+
+    override def start(): IO[Unit] = {
+      val role = winnerCrew match {
+        case _: Crewmate => "Crewmate"
+        case _: Impostor => "Impostor"
+      }
+      for {
+        menuBorder <- BorderFactoryIO.emptyBorderCreated(spaceDimension10,
+          spaceDimension10, spaceDimension10, spaceDimension10)
+        _ <- votePanel.setBorder(menuBorder)
+        _ <- votePanel.setLayout(new BorderLayout())
+
+        text <- JLabelIO()
+        borderText <- BorderFactoryIO.emptyBorderCreated(0, spaceDimension60, 0, 0)
+        _ <- text.setBorder(borderText)
+        _ <- text.setText("The Winning Team is: " + role)
+
+        _ <- votePanel.add(text, BorderLayout.CENTER)
+
+        cp <- frame.contentPane()
+        _ <- cp.add(votePanel)
+        _ <- frame.setResizable(false)
+        _ <- frame.setTitle("Among Sus - Results")
+        _ <- frame.setSize(WIDTH/3, HEIGHT/3)
+        _ <- frame.setVisible(true)
+      } yield ()
+    }
 
     override def dispose(): IO[Unit] = for {
       _ <- frame.dispose()
