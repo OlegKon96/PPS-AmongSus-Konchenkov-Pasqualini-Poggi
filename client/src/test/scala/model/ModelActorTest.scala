49e10ef0331731880b0340a1c82dcc98b982cc9e
Oleg Konchenkov <konchenkovoleg@gmail.com>
2021-03-06 15:56:29 +0100

    Create ModelActorTest.scala

---
+++ client/src/test/scala/model/ModelActorTest.scala
@@ -0,0 +1,53 @@
+package model
+
+import akka.actor.{ActorRef, ActorSystem}
+import akka.testkit.{ImplicitSender, TestKit, TestProbe}
+import com.typesafe.config.ConfigFactory
+import it.amongsus.controller.ActionTimer.TimerStarted
+import it.amongsus.controller.actor.ControllerActorMessages.{ButtonOffController, UpdatedMyCharController, UpdatedPlayersController}
+import it.amongsus.controller.actor.{ControllerActor, LobbyActorInfo}
+import it.amongsus.core.entities.map.{Collectionable, Tile}
+import it.amongsus.core.entities.player.{CrewmateAlive, ImpostorAlive, Player}
+import it.amongsus.core.entities.util.Movement.Up
+import it.amongsus.core.entities.util.Point2D
+import it.amongsus.model.actor.ModelActorMessages.{KillTimerStatusModel, MyCharMovedModel}
+import it.amongsus.model.actor.{ModelActor, ModelActorInfo}
+import org.scalatest.BeforeAndAfterAll
+import org.scalatest.wordspec.AnyWordSpecLike
+
+class ModelActorTest extends TestKit(ActorSystem("test", ConfigFactory.load("test")))
+  with ImplicitSender
+  with AnyWordSpecLike
+  with BeforeAndAfterAll {
+
+  override protected def afterAll(): Unit = TestKit.shutdownActorSystem(system)
+
+  private val modelActorInfo: ModelActorInfo = ModelActorInfo()
+  private val map: Array[Array[Tile]] = modelActorInfo.generateMap(loadMap())
+  modelActorInfo.generateCollectionables(map)
+  val players = Seq(CrewmateAlive("green", emergencyCalled = false, "test", "test", 0, Point2D(35, 35)))
+
+  "Get Ready, Win a Match and then Leave the Client" in {
+    val controller = TestProbe()
+
+    val modelActor = system.actorOf(ModelActor.props(ModelActorInfo(Option(controller.ref), Option(map), players,
+      modelActorInfo.gameCollectionables, "test")))
+
+    modelActor ! KillTimerStatusModel(TimerStarted)
+    controller.expectMsgType[ButtonOffController]
+
+    modelActor ! MyCharMovedModel(Up())
+    controller.expectMsgType[ButtonOffController]
+    controller.expectMsgType[ButtonOffController]
+    controller.expectMsgType[ButtonOffController]
+    controller.expectMsgType[ButtonOffController]
+    controller.expectMsgType[UpdatedMyCharController]
+    controller.expectMsgType[UpdatedPlayersController]
+
+  }
+
+  def loadMap(): Iterator[String] = {
+    val bufferedSource = scala.io.Source.fromInputStream(getClass.getResourceAsStream("/images/gameMap.csv"))
+    bufferedSource.getLines
+  }
+}
