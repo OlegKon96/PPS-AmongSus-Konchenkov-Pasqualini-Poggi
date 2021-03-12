8dd1bd612485c4f68f97477392fe6a3e79cebb44
Giovanni <giovanni.poggi2@studio.unibo.it>
2021-03-12 11:23:50 +0100

    Update GameActor and LobbyManagerActor
    Update and Refactor Game Actor, GameActor Info, Lobby Manager, Lobby Manager Actor and some tests

--- server/src/main/scala/it/amongsus/server/game/GameActorInfo.scala
+++ server/src/main/scala/it/amongsus/server/game/GameActorInfo.scala
@@ -1,6 +1,13 @@
 package it.amongsus.server.game
 
+import akka.actor.{ActorRef, PoisonPill}
+import it.amongsus.core.player.{AlivePlayer, Constants, Crewmate, CrewmateAlive, Impostor, ImpostorAlive, Player}
+import it.amongsus.core.util.GameEnd.{CrewmateCrew, ImpostorCrew, Lost, Win}
+import it.amongsus.core.util.{Point2D, WinnerCrew}
+import it.amongsus.messages.GameMessageClient.GameEndClient
 import it.amongsus.server.common.GamePlayer
+import it.amongsus.utils.CustomLogger
+import scala.util.Random
 
 /**
  * Trait of the info of Game Actor
@@ -22,6 +29,19 @@ trait GameActorInfo {
    * Map Username-Vote of all players in the lobby
    */
   var playersToLobby: Map[String, Int]
+  /**
+   *
+   * @param gamePlayers
+   * @param crew
+   * @param gameActor
+   */
+  def sendWinMessage(gamePlayers: Seq[Player], crew: WinnerCrew, gameActor: ActorRef): Unit
+  def defineRoles(): Seq[Player]
+  def broadcastMessageToPlayers(message: Any): Unit
+  def withPlayer(playerId: String)(f: GamePlayer => Unit): Unit
+  def checkWinCrewmate(gamePlayers: Seq[Player]): Boolean
+  def checkAllCoinsCollected(gamePlayers: Seq[Player]): Boolean
+  def checkWinImpostor(gamePlayers: Seq[Player]): Boolean
 }
 
 object GameActorInfo {
@@ -33,7 +53,84 @@ object GameActorInfo {
 
 case class GameActorInfoData(override var players: Seq[GamePlayer], override var totalVotes: Int,
                              override var playersToLobby: Map[String,Int], override val numberOfPlayers: Int)
-                            extends GameActorInfo {
+                            extends GameActorInfo with CustomLogger {
   this.totalVotes = this.numberOfPlayers
   this.playersToLobby = Map.empty
+  private final val mapCenter: Int = 35
+  private final val colorsSequence: Seq[String] = Seq("green", "red", "cyan", "yellow", "blue", "pink", "orange")
+
+  override def sendWinMessage(gamePlayers: Seq[Player], crew: WinnerCrew, gameActor: ActorRef): Unit = {
+    gamePlayers.foreach{
+      case c : Crewmate =>
+        crew match {
+          case ImpostorCrew() => this.players.find(p => p.id == c.clientId).get.actorRef !
+            GameEndClient(Lost(gamePlayers.filter(player => player.isInstanceOf[Impostor]), crew))
+          case CrewmateCrew() => this.players.find(p => p.id == c.clientId).get.actorRef !
+            GameEndClient(Win(gamePlayers.filter(player => player.isInstanceOf[Crewmate]), crew))
+        }
+      case i : Impostor =>
+        crew match {
+          case ImpostorCrew() => this.players.find(p => p.id == i.clientId).get.actorRef !
+            GameEndClient(Win(gamePlayers.filter(player => player.isInstanceOf[Impostor]), crew))
+          case CrewmateCrew() => this.players.find(p => p.id == i.clientId).get.actorRef !
+            GameEndClient(Lost(gamePlayers.filter(player => player.isInstanceOf[Crewmate]), crew))
+        }
+    }
+    log("Game ended...")
+    gameActor ! PoisonPill
+  }
+
+  override def defineRoles(): Seq[Player] = {
+    var playersRole: Seq[Player] = Seq()
+    val rand1 = Random.nextInt(this.players.length)
+    val colors = Random.shuffle(colorsSequence)
+    val rand2 = if(this.players.length > 5) Random.nextInt(this.players.length) else rand1
+    val mapCentre = Point2D(mapCenter, mapCenter)
+
+    for (n <- this.players.indices) {
+      n match {
+        case n if n == rand1 || n == rand2 =>
+          playersRole = playersRole :+ ImpostorAlive(colors(n), emergencyCalled = false, this.players(n).id,
+            this.players(n).username, mapCentre)
+        case _ => playersRole = playersRole :+ CrewmateAlive(colors(n), emergencyCalled = false,
+          this.players(n).id, this.players(n).username, Constants.Crewmate.NUM_COINS, mapCentre)
+      }
+    }
+    playersRole
+  }
+
+  /**
+   * Broadcast a generic message to all game players
+   *
+   * @param message a generic message
+   */
+  override def broadcastMessageToPlayers(message: Any): Unit = {
+    players.foreach(p => p.actorRef ! message)
+  }
+
+  override def withPlayer(playerId: String)(f: GamePlayer => Unit): Unit = {
+    players.find(_.id == playerId) match {
+      case Some(p) => f(p)
+      case None => log(s"Player id $playerId not found")
+    }
+  }
+
+  override def checkWinCrewmate(gamePlayers: Seq[Player]): Boolean = {
+    gamePlayers.count(player => player.isInstanceOf[ImpostorAlive]) == 0 || checkAllCoinsCollected(gamePlayers)
+  }
+
+  override def checkAllCoinsCollected(gamePlayers: Seq[Player]): Boolean = {
+    var count = 0
+    gamePlayers.foreach {
+      case p: Crewmate =>
+        if (p.numCoins == 10) count += 1
+      case _ =>
+    }
+    if (count == gamePlayers.count(player => player.isInstanceOf[Crewmate])) true else false
+  }
+
+  override def checkWinImpostor(gamePlayers: Seq[Player]): Boolean = {
+    gamePlayers.count(player =>
+      player.isInstanceOf[AlivePlayer]) <= gamePlayers.count(player => player.isInstanceOf[ImpostorAlive]) * 2
+  }
 }
