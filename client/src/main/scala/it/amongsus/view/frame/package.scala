2a161677605e00b19d64af2030aa34c3ee225c89
Oleg Konchenkov <konchenkovoleg@gmail.com>
2021-03-13 12:02:37 +0100

    Create package.scala

---
+++ client/src/main/scala/it/amongsus/view/frame/package.scala
@@ -0,0 +1,33 @@
+package it.amongsus.view
+
+package object frame {
+
+  object Constants{
+
+    object VoteFrame{
+      object Strings{
+
+      }
+      object Numbers{
+
+      }
+    }
+
+    object WinFrama{
+
+    }
+
+    object GameFrame{
+
+    }
+
+    object  LobbyFrame{
+
+    }
+
+    object MenuFrame{
+
+    }
+
+  }
+}
